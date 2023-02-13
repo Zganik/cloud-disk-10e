9646c4521a05385f7dc653ee050118368124225e
Zganik <118707971+Zganik@users.noreply.github.com>
2023-02-13 08:50:12 +0600

    lesson 5

---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,16 @@
+
+
+const defaultState = {
+  currentUser: {},
+  isAuth: false
+}
+
+export default function userReducer(state = defaultState, action) {
+  
+  switch (action.type) {
+
+    default: 
+      return state
+  }
+
+}
